0c629171b46c9bf706b93bdfac4f8c203ef368eb
Роман <rk-nk-sd@bk.ru>
2020-12-02 00:49:12 +0700

    Реализовал подключение к БД - Util.java

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,12 @@
 package jm.task.core.jdbc;
 
+import com.mysql.jdbc.Connection;
+import jm.task.core.jdbc.util.Util;
+
 public class Main {
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
+         Connection connection = new Util().getConnectionFromDataBase();
+        System.out.println(connection.toString());
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,37 @@
 package jm.task.core.jdbc.util;
 
+import com.mysql.jdbc.Connection;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
 public class Util {
     // реализуйте настройку соеденения с БД
+
+    /**
+     * Конфигурация для подключения к БД
+     */
+    private final String url = "jdbc:mysql://127.0.0.1/jmDatabase";
+    private final String user = "jm";
+    private final String pass = "Pa$$word";
+
+    private static Connection connection;
+
+    public Connection getConnectionFromDataBase() {
+        try {
+            /**
+             * Открываем соединение с БД
+             */
+            connection = (Connection) DriverManager.getConnection(url, user, pass);
+        } catch (SQLException sqle) {
+            sqle.printStackTrace();
+        } finally {
+            try {
+                if (connection != null)
+                connection.close();
+            } catch(SQLException se) {  }
+        }
+        return connection;
+    }
 }
